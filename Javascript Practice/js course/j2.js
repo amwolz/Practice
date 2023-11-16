dffeaff48a669e2a1f2173e1ca11259c0c9cc7f4
amwolz <amw21@rice.edu>
2023-11-16 10:12:01 -0700

    m

--- Javascript Practice/js course/j2.js
+++ Javascript Practice/js course/j2.js
@@ -38,3 +38,21 @@ function isValidEmail(email){
 
 console.log(happyBirthday("Andrew", 21));
 console.log(isValidEmail("email@fake.com"));
+
+// variable scope = where a variable is recognized
+//                               and accessible (local vs global)            
+
+let x = 3; // global scope
+
+function1();
+function2();
+
+function function1(){
+    let x = 1; // local scope
+    console.log(x);
+}
+
+function function2(){
+    let x = 2; // local scope
+    console.log(x);
+}
