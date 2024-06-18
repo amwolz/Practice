fbfe9cabf288c6f760e30260f3685ceb7a637108
amwolz <amw21@rice.edu>
2024-06-18 13:16:24 -0500

    graphs and hashing

---
+++ Algorithms and Data Structures Practice/graphs.py
@@ -0,0 +1,111 @@
+# Matrix (2D Grid)
+grid = [[0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0]]
+
+# Adjacency matrix
+adjMatrix = [[0, 0, 0, 0],
+             [1, 1, 0, 0],
+             [0, 0, 0, 1],
+             [0, 1, 0, 0]]
+
+# GraphNode used for adjacency list
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbors = []
+
+# Matrix (2D Grid)
+grid = [[0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0]]
+
+# Count paths (backtracking)
+def dfs(grid, r, c, visit):
+    ROWS, COLS = len(grid), len(grid[0])
+    if (min(r, c) < 0 or
+        r == ROWS or c == COLS or
+        (r, c) in visit or grid[r][c] == 1):
+        return 0
+    if r == ROWS - 1 and c == COLS - 1:
+        return 1
+
+    visit.add((r, c))
+
+    count = 0
+    count += dfs(grid, r + 1, c, visit)
+    count += dfs(grid, r - 1, c, visit)
+    count += dfs(grid, r, c + 1, visit)
+    count += dfs(grid, r, c - 1, visit)
+
+    visit.remove((r, c))
+    return count
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        islands = 0
+        visit = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (
+                r not in range(rows)
+                or c not in range(cols)
+                or grid[r][c] == "0"
+                or (r, c) in visit
+            ):
+                return
+
+            visit.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    islands += 1
+                    dfs(r, c)
+        return islands
+
+
+# BFS Version From Video
+class SolutionBFS:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visited=set()
+        islands=0
+
+         def bfs(r,c):
+             q = deque()
+             visited.add((r,c))
+             q.append((r,c))
+           
+             while q:
+                 row,col = q.popleft()
+                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+                 for dr,dc in directions:
+                     r,c = row + dr, col + dc
+                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                       
+                         q.append((r , c ))
+                         visited.add((r, c ))
+
+         for r in range(rows):
+             for c in range(cols):
+               
+                 if grid[r][c] == "1" and (r,c) not in visited:
+                     bfs(r,c)
+                     islands +=1 
+
+         return islands
+
